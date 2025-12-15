8e85f2a557aa1d0a2f543695508554b1f5ced882
Soulcatcher78 <ivanvhik8787@gmail.com>
2025-12-15 12:51:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
+  const savedTasks = localStorage.getItem("tasks");
 
+  if (savedTasks) {
+    return JSON.parse(savedTasks);
+  }
+
+  return items;
 }
 
 function createItem(item) {
@@ -22,14 +28,67 @@ function createItem(item) {
   const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
   const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
   const editButton = clone.querySelector(".to-do__item-button_type_edit");
+  textElement.textContent = item;
+  deleteButton.addEventListener("click", () => {
+  clone.remove();
 
+  const items = getTasksFromDOM();
+  saveTasks(items);
+});
+  duplicateButton.addEventListener("click", () => {
+  const itemName = textElement.textContent;
+  const newItem = createItem(itemName);
+
+  listElement.prepend(newItem);
+
+  const items = getTasksFromDOM();
+  saveTasks(items);
+});
+  editButton.addEventListener("click", () => {
+  textElement.setAttribute("contenteditable", "true");
+  textElement.focus();
+});
+  textElement.addEventListener("blur", () => {
+  textElement.setAttribute("contenteditable", "false");
+
+  const items = getTasksFromDOM();
+  saveTasks(items);
+});
+  return clone;
 }
 
 function getTasksFromDOM() {
+  const itemsNamesElements = document.querySelectorAll(".to-do__item-text");
+  const tasks = [];
 
+  itemsNamesElements.forEach((element) => {
+    tasks.push(element.textContent);
+  });
+
+  return tasks;
 }
 
 function saveTasks(tasks) {
-
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+items = loadTasks();
+
+items.forEach((item) => {
+  const taskElement = createItem(item);
+  listElement.append(taskElement);
+});
+formElement.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const item = inputElement.value;
+
+  const taskElement = createItem(item);
+  listElement.prepend(taskElement);
+
+  items = getTasksFromDOM();
+  saveTasks(items);
+
+  inputElement.value = "";
+});
 
